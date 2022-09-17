6d6f770e8716ba94cbe6f0e13a118f85ad348ab7
Obarda <73034911+Obarda@users.noreply.github.com>
2022-09-17 19:10:38 +0300

    Update rename.py

--- rename.py
+++ rename.py
@@ -8,7 +8,6 @@ counter = 0
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if str.isdigit(file[0]) and file[1] == '_':
-            # print(file, " - fixed!")
             counter = counter + 1
             os.chdir(subdir)
             os.rename(file, file[2:])
