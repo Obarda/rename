42f9a9a696c4adae99b0a80719cc43acf250de45
Obarda <73034911+Obarda@users.noreply.github.com>
2022-09-17 17:16:33 +0300

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,17 @@
+import os
+
+print("Enter path:")
+rootdir = input ()
+
+counter = 0
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if str.isdigit(file[0]) and file[1] == '_':
+            # print(file, " - fixed!")
+            counter = counter + 1
+            os.chdir(subdir)
+            os.rename(file, file[2:])
+
+print("Converting ", counter, "files...")
+print("Done!")
